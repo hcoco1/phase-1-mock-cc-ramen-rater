0a65007c8f26bbd2d05861b39dc76da8bfde6116
Ivan <arias.ivan@gmail.com>
2023-04-25 21:56:00 -0400

    core deliverables done

--- src/index.js
+++ src/index.js
@@ -1,1 +1,41 @@
-// write your code here
+/* See all ramen images in the div with the id of ramen-menu. 
+When the page loads, request the data from the server to get all the ramen objects.
+Then, display the image for each of the ramen using an img tag inside the #ramen-menu div. */
+
+fetch("http://localhost:3000/ramens")
+  .then((res) => res.json())
+  .then((data) => handleRamen(data));
+
+function handleRamen(data) {
+  //console.log(data);
+  data.forEach(function (val) {
+    let ramenMenu = document.querySelector("#ramen-menu");
+    let ramenImage = document.createElement("img");
+    //console.log(val);
+    ramenImage.src = val.image;
+    ramenMenu.appendChild(ramenImage);
+
+    /* Click on an image from the #ramen-menu div and see all the
+info about that ramen displayed inside the #ramen-detail div
+and where it says insert comment here and insert rating here. */
+
+    ramenImage.addEventListener("click", () => {
+      let detailsImage = document.querySelector(".detail-image");
+      detailsImage.src = ramenImage.src;
+
+      let ramenName = document.querySelector("h2");
+      ramenName.innerText = val.name;
+
+      let ramenRestaurant = document.querySelector(".restaurant");
+      ramenRestaurant.innerText = val.restaurant;
+
+      let ramenRating = document.querySelector('#rating-display');
+      //console.log(ramenRating)
+      ramenRating.innerText = val.rating;
+
+      let ramenComments = document.querySelector('#comment-display');
+      //console.log(ramenComments)
+      ramenComments.innerText = val.comment;
+    });
+  });
+}
