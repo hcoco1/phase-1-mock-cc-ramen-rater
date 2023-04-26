b0a145e186b32342f4f016201ffd44c2d931a1f0
Ivan <arias.ivan@gmail.com>
2023-04-25 22:49:17 -0400

    new form

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ fetch("http://localhost:3000/ramens")
   .then((data) => handleRamen(data));
 
 function handleRamen(data) {
-  //console.log(data);
+  console.log(typeof(data));
   data.forEach(function (val) {
     let ramenMenu = document.querySelector("#ramen-menu");
     let ramenImage = document.createElement("img");
@@ -29,13 +29,52 @@ and where it says insert comment here and insert rating here. */
       let ramenRestaurant = document.querySelector(".restaurant");
       ramenRestaurant.innerText = val.restaurant;
 
-      let ramenRating = document.querySelector('#rating-display');
+      let ramenRating = document.querySelector("#rating-display");
       //console.log(ramenRating)
       ramenRating.innerText = val.rating;
 
-      let ramenComments = document.querySelector('#comment-display');
+      let ramenComments = document.querySelector("#comment-display");
       //console.log(ramenComments)
       ramenComments.innerText = val.comment;
     });
   });
 }
+
+/* Create a new ramen after submitting the new-ramen form.
+The new ramen should be added to the#ramen-menu div.
+The new ramen does not need to persist; in other words,
+if you refresh the page, it's okay that the new ramen
+is no longer on the page. */
+
+let submitForm = document.querySelector("#new-ramen")
+submitForm.addEventListener("submit", (e) =>{
+    e.preventDefault()
+
+  let newNameRamenInput = document.querySelector("#new-name");
+  //console.log(newNameRamenInput)
+
+  let newRestaurantRamenInput = document.querySelector("#new-restaurant");
+  //console.log(newRestaurantRamenInput)
+
+  let newImageRamenInput = document.querySelector("#new-image");
+  //console.log(newImageRamenInput)
+
+  let newRatingRamenInput = document.querySelector("#new-rating");
+  //console.log(newRatingRamenInput)
+
+  let newCommentRamenInput = document.querySelector("#new-comment");
+  //console.log(newCommentRamenInput)
+
+  const ramenObj = {
+    name: newNameRamenInput.value,
+    restaurant: newRestaurantRamenInput.value,
+    image: newImageRamenInput.value,
+    rating: newRatingRamenInput.value,
+    comment: newCommentRamenInput.value,
+  };
+
+ handleRamen([ramenObj])
+})
+
+ 
+
